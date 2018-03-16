3286c6c666c59060670d384464c13c5c9779d785
Ayush Tulsyan <ayushtuls.64@gmail.com>
2018-03-16 23:01:58 +0530

    Minor changes to improve performance
    Added memoization for multiplication and exponentiation
    Changed index value alignment in INVERSE array

--- Assignment5/code/utilities.py
+++ Assignment5/code/utilities.py
@@ -5,6 +5,8 @@
 ################################################################################
 
 IRREDUCIBLE_POL = [1, 0, 0, 0, 0, 0, 1, 1]
+mult_cache = [[-1]*128 for i in range(128)]
+expo_cache = [[-1]*128 for i in range(128)]
 
 #  A GF_128 element will be represented as 7 bit integer
 #  ie x^2+x+1 will be represented as 0000111 in binary
@@ -18,6 +20,9 @@ def Add (elem1, elem2):
     return elem1 ^ elem2
 
 def Multiply (elem1, elem2):
+    if mult_cache[elem1][elem2] != -1:
+        return mult_cache[elem1][elem2]
+
     elem1 = int(elem1)
     elem2 = int(elem2)
     elem3 = 0
@@ -39,9 +44,15 @@ def Multiply (elem1, elem2):
     #  This is same as (x^5 + x^4 + .. + 1)*(x+1)+..
     upper = elem3 >> 7
     product = Add(Add(upper, upper<<1), elem3 & GF128_MASK)
+
+    mult_cache[elem1>>7][elem2] = product
+    mult_cache[elem2][elem1>>7] = product
     return product
 
 def Exponentiate (elem, power):
+    if expo_cache[elem][power] != -1:
+        return expo_cache[elem][power]
+
     result = 0;
     if power == 0:
         result = 1
@@ -54,6 +65,8 @@ def Exponentiate (elem, power):
         sqrt_elem = Exponentiate(elem, power>>1)
         result = Multiply(sqrt_elem, sqrt_elem)
         result = Multiply(elem, result)
+
+    expo_cache[elem][power] = result
     return result
 
 def LinearTransform (matrix, elem_list):
@@ -82,7 +95,8 @@ def LinearTransform (matrix, elem_list):
 #  Other Functions
 ################################################################################
 
-INVERSE = [1, 64, 85, 32, 51, 106, 109, 16, 113, 89, 104, 53, 88, 118, 17, 8, 15, 120, 107, 108, 121, 52, 116, 90, 61, 44, 80, 59, 92, 72, 41, 4, 77, 71, 98, 60, 103, 117, 114, 54, 31, 124, 65, 26, 48, 58, 100, 45, 70, 94, 5, 22, 12, 40, 97, 93, 78, 46, 28, 36, 25, 84, 125, 2, 43, 102, 91, 99, 81, 49, 34, 30, 87, 115, 105, 122, 33, 57, 82, 27, 69, 79, 101, 62, 3, 96, 73, 13, 10, 24, 67, 29, 56, 50, 123, 86, 55, 35, 68, 47, 83, 66, 37, 11, 75, 6, 19, 20, 7, 112, 119, 110, 9, 39, 74, 23, 38, 14, 111, 18, 21, 76, 95, 42, 63, 126]
+# -1 (As inverse of 0 or 127) to have proper index value mapping
+INVERSE = [-1, 1, 64, 85, 32, 51, 106, 109, 16, 113, 89, 104, 53, 88, 118, 17, 8, 15, 120, 107, 108, 121, 52, 116, 90, 61, 44, 80, 59, 92, 72, 41, 4, 77, 71, 98, 60, 103, 117, 114, 54, 31, 124, 65, 26, 48, 58, 100, 45, 70, 94, 5, 22, 12, 40, 97, 93, 78, 46, 28, 36, 25, 84, 125, 2, 43, 102, 91, 99, 81, 49, 34, 30, 87, 115, 105, 122, 33, 57, 82, 27, 69, 79, 101, 62, 3, 96, 73, 13, 10, 24, 67, 29, 56, 50, 123, 86, 55, 35, 68, 47, 83, 66, 37, 11, 75, 6, 19, 20, 7, 112, 119, 110, 9, 39, 74, 23, 38, 14, 111, 18, 21, 76, 95, 42, 63, 126, -1]
 
 def EncodeBlock(plain):
     if len(plain) != 8:
